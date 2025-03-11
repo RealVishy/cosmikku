e6f05ab92860a739d98308368e1d78a3e7f404d9
Vishwas Parpattegar <vishwasparpattegar@gmail.com>
2025-03-11 22:34:41 +1100

    add cosmic desktop

--- build.sh
+++ build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 copr enable -y ryanabx/cosmic-epoch
+dnf5 -y install @cosmic-desktop @cosmic-desktop-apps
+dnf5 clean all 
 
 # Use a COPR Example:
 #
